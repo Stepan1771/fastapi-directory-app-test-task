87784784d7a4abb361904f0ea5b445b3a322c150
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-10-22 21:01:22 +0300

    create abstract class

---
+++ directory-app/core/models/base.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from core.config import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
