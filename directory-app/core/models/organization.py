e313836d3458cfa5a97cfa2b5f112d2b00aaa3ad
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-10-23 14:08:26 +0300

    create organizations table

---
+++ directory-app/core/models/organization.py
@@ -0,0 +1,35 @@
+from sqlalchemy import String, ForeignKey, ARRAY
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base, organization_activity
+
+
+class Organization(Base):
+    __tablename__ = 'organizations'
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+        index=True,
+    )
+    name: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        unique=True,
+    )
+    phone_numbers: Mapped[int] = mapped_column(
+        ARRAY(String(20)),
+        nullable=False,
+        default=[],
+    )
+    building_id: Mapped[int] = mapped_column(
+        ForeignKey('buildings.id'),
+        nullable=False,
+    )
+
+    building = relationship("Building", back_populates="organizations")
+    activities = relationship(
+        "Activity",
+        secondary=organization_activity,
+        back_populates="organizations"
+    )
