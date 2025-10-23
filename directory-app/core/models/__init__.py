13f1b5b877d3a66d81975337b8169d6c71292134
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-10-23 22:08:20 +0300

    add OrganizationActivity

--- directory-app/core/models/__init__.py
+++ directory-app/core/models/__init__.py
@@ -4,6 +4,7 @@ all = (
     "Organization",
     "Building",
     "Activity",
+    "OrganizationActivity",
 )
 
 from .db_helper import db_helper
@@ -11,3 +12,4 @@ from .base import Base
 from .organization import Organization
 from .building import Building
 from .activity import Activity
+from .organization_activity import OrganizationActivity
