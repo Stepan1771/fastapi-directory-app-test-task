5e7820dc262c1d9e3b2ce6155aeea46807c26ba2
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-10-23 14:06:56 +0300

    init all tables

--- directory-app/core/models/__init__.py
+++ directory-app/core/models/__init__.py
@@ -1,5 +1,13 @@
 all = (
     "db_helper",
+    "Base",
+    "Organization",
+    "Building",
+    "Activity",
 )
 
 from .db_helper import db_helper
+from .base import Base
+from .organization import Organization
+from .building import Building
+from .activity import Activity
