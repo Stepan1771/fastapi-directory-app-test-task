f9ac8444d4a756e69f90fc3b7a1b9465804d14b1
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-10-25 02:46:18 +0300

    change

--- directory-app/core/schemas/activity.py
+++ directory-app/core/schemas/activity.py
@@ -1,12 +1,10 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 
 class Activity(BaseModel):
     id: int
     name: str
-    parent_id: Optional[int]
+    parent_id: int | None
     level: int
 
     class Config:
