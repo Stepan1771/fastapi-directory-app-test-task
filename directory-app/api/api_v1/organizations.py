16b66befdcde790caf8be13b220009a217d44add
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-10-24 14:04:34 +0300

    create get_organizations_in_radius, get_organizations_by_id, get_organizations_by_activity_type, get_organizations_by_name endpoints

--- directory-app/api/api_v1/organizations.py
+++ directory-app/api/api_v1/organizations.py
@@ -11,6 +11,8 @@ from core.schemas.organization import Organization
 
 import crud.organizations as crud_organizations
 
+from core.schemas.search_radius import SearchRadius
+
 router = APIRouter(
     prefix=settings.api.v1.organizations,
     tags=["Organizations"],
@@ -53,5 +55,85 @@ async def get_organizations_by_activity(
         session=session,
         api_key=api_key,
         activity_id=activity_id,
+    )
+    return result
+
+
+@router.post("/get-organizations-in-radius")
+async def get_organizations_in_radius(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+        api_key: Annotated[
+            str,
+            Depends(verify_api_key),
+        ],
+        search_radius: SearchRadius,
+):
+    result = await crud_organizations.get_all_organizations_in_radius(
+        session=session,
+        api_key=api_key,
+        search_radius=search_radius,
+    )
+    return result
+
+
+@router.get("/get-organizations-by-id")
+async def get_organizations_by_id(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+        api_key: Annotated[
+            str,
+            Depends(verify_api_key),
+        ],
+        organization_id: int,
+):
+    result = await crud_organizations.get_organization_by_id(
+        session=session,
+        api_key=api_key,
+        organization_id=organization_id,
+    )
+    return result
+
+
+@router.get("/get-organizations-by-activity-type")
+async def get_organizations_by_activity_type(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+        api_key: Annotated[
+            str,
+            Depends(verify_api_key),
+        ],
+        activity_id: int,
+):
+    result = await crud_organizations.search_organizations_by_activity_type(
+        session=session,
+        api_key=api_key,
+        activity_id=activity_id,
+    )
+    return result
+
+
+@router.get("/get-organizations-by-name")
+async def get_organizations_by_name(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+        api_key: Annotated[
+            str,
+            Depends(verify_api_key),
+        ],
+        organization_name: str,
+):
+    result = await crud_organizations.search_organizations_by_name(
+        session=session,
+        api_key=api_key,
+        organization_name=organization_name,
     )
     return result
