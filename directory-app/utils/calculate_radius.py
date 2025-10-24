3413879cf35afbbd7e0e4291e020f89d1fb951fe
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-10-25 02:47:47 +0300

    fixed calculate_distance func

--- directory-app/utils/calculate_radius.py
+++ directory-app/utils/calculate_radius.py
@@ -1,22 +1,15 @@
-from math import radians, sin, cos, sqrt, atan2
+import math
 
 
 def calculate_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
-    """
-    Calculate distance between two points on Earth in kilometers
-    using Haversine formula
-    """
-    R = 6371  # Earth radius in kilometers
+    R = 6371  # Радиус Земли в км
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lon2 - lon1)
 
-    lat1_rad = radians(lat1)
-    lon1_rad = radians(lon1)
-    lat2_rad = radians(lat2)
-    lon2_rad = radians(lon2)
-
-    dlat = lat2_rad - lat1_rad
-    dlon = lon2_rad - lon1_rad
-
-    a = sin(dlat / 2) ** 2 + cos(lat1_rad) * cos(lat2_rad) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
+    a = math.sin(delta_phi / 2) ** 2 + \
+        math.cos(phi1) * math.cos(phi2) * \
+        math.sin(delta_lambda / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     return R * c
